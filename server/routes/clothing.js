53c67e4b62bf5d3f3151b17e435f5418c7ac7970
chandanch <chandan.cec107@gmail.com>
2020-09-27 18:47:23 +0530

    change readfile to async :recycle:

--- server/routes/clothing.js
+++ server/routes/clothing.js
@@ -1,17 +1,20 @@
-const express = require('express');
-const fs = require('fs');
-const datafile = 'server/data/clothing.json';
+const express = require("express");
+const fs = require("fs");
+const datafile = "server/data/clothing.json";
 const router = express.Router();
 
 /* GET all clothing */
-router.route('/')
-  .get(function(req, res) {
-
-    let rawData = fs.readFileSync(datafile, 'utf8');
-    let clothingData = JSON.parse(rawData);
-
-    res.send(clothingData);
-
+router.route("/").get(function (req, res) {
+  fs.readFile(datafile, "utf-8", (err, data) => {
+    if (err) {
+      console.log("Error in reading file");
+    } else {
+      const clothingData = JSON.parse(data);
+      console.log("Returning clothing data");
+      res.send(clothingData);
+    }
   });
+  console.log("Doing more work...");
+});
 
 module.exports = router;
